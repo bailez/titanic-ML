5b121311c0c5fc49f6584660d67fee13b1ab4d76
bailez <bailez@usp.br>
2019-10-13 01:37:49 -0300

    Testes julia

--- scripts/julia/test.jl
+++ scripts/julia/test.jl
@@ -1,5 +1,5 @@
 using DataFrames, CSV
-test = CSV.read("/run/media/bailez/Dados/Projetos e Serviços/titanic-resolutions/data/test.csv");
-print(test);
+test = CSV.read("/run/media/bailez/Dados/Projetos e Serviços/titanic-resolutions/data/test.csv")
+print(test)
 print(head(test))
 print("\n",typeof(test))
