d249f4aeaaf10d09c1982535f9c543e948950c93
bailez <bailez@usp.br>
2019-10-14 22:27:56 -0300

    drop columns in julia

--- scripts/julia/test.jl
+++ scripts/julia/test.jl
@@ -1,5 +1,7 @@
 using DataFrames, CSV
-test = CSV.read("/run/media/bailez/Dados/Projetos e Serviços/titanic-resolutions/data/test.csv")
-print(test)
-print(head(test))
-print("\n",typeof(test))
+test = CSV.read("/run/media/bailez/Dados/Projetos e Serviços/titanic-ML/data/test.csv")
+train = CSV.read("/run/media/bailez/Dados/Projetos e Serviços/titanic-ML/data/train.csv")
+survived = train[:Survived]
+deletecols!(train,[:Name,:Ticket,:Cabin ])
+deletecols!(test,[:Name,:Ticket,:Cabin ])
+print(head(train), "\n", head(test))
