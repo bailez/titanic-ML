c5f3eb395d7c97d8a7d401ec1366923eca683282
bailez <bailez@usp.br>
2019-10-20 14:54:42 -0300

    decision tree submission

--- scripts/python/decision_tree.py
+++
@@ -1,7 +0,0 @@
-#Python solution using decision tree
-from utils import get_data
-import pandas as pd
-import os
-from sklearn.linear_model import LinearRegression
-
-train, test = get_data()
---
+++ scripts/python/tree.py
@@ -0,0 +1,23 @@
+#Python solution using decision tree
+from utils import get_data
+import pandas as pd
+import os
+from sklearn.tree import DecisionTreeClassifier
+
+
+train, test = get_data()
+# %% Concatenate train and test data into a df
+#reg = LinearRegression()
+clf = DecisionTreeClassifier()
+clf.fit(train.drop(columns=['Survived']),train.Survived)
+print(clf.score(train.drop(columns=['Survived']), train.Survived))
+# %% Make prediction and transform in logistic
+prediction = clf.predict(test)
+prediction = pd.Series(prediction).apply(lambda x: 0 if x<0.5 else 1)
+# %% Create Dataframe for submission
+output = {'PassengerId' : test.PassengerId, 'Survived': prediction}
+df = pd.DataFrame(output)
+# %%
+print(df)
+os.chdir(r'../submissions')
+df.to_csv('tree-py.csv')
